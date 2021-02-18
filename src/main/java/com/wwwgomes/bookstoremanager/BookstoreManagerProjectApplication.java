d78057550e90d8949b451e202ac71960dc5d3949
Williams Gomes <williamsgomess@outlook.com>
2021-02-18 13:59:03 -0300

    First commit

---
+++ src/main/java/com/wwwgomes/bookstoremanager/BookstoreManagerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wwwgomes.bookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreManagerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreManagerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wwwgomes/bookstoremanager/BookstoreManagerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwwgomes.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreManagerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
