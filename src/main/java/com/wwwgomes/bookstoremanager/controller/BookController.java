f043ae38bd8d8fb284bafe4418855c041b119881
Williams Gomes <williamsgomess@outlook.com>
2021-02-18 18:37:04 -0300

    Changed Controller for example

--- src/main/java/com/wwwgomes/bookstoremanager/controller/BookController.java
+++ src/main/java/com/wwwgomes/bookstoremanager/controller/BookController.java
@@ -17,6 +17,6 @@ public class BookController {
     })
     @GetMapping
     public String hello() {
-        return "Hello Bookstore Manager";
+        return "Hello Bookstore Manager, I am running an example with PR!!";
     }
 }
