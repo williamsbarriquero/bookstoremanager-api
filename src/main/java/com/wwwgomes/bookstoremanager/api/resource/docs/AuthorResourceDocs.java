3e02bb199f91b659b9ec031096b1fa14b589dc52
Williams Gomes <williamsgomess@outlook.com>
2021-02-24 21:44:27 -0300

    Developed author by id functionality

--- src/main/java/com/wwwgomes/bookstoremanager/api/resource/docs/AuthorResourceDocs.java
+++ src/main/java/com/wwwgomes/bookstoremanager/api/resource/docs/AuthorResourceDocs.java
@@ -11,9 +11,16 @@ public interface AuthorResourceDocs {
 
     @ApiOperation(value = "Author creation operation")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Success author cration"),
+            @ApiResponse(code = 201, message = "Success author creation"),
             @ApiResponse(code = 400,
                     message = "Missing required fields, wrong field range value or author already registered on system")
     })
     AuthorDTO create(AuthorDTO authorDTO);
+
+    @ApiOperation(value = "Find author by id operation.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Success author found"),
+            @ApiResponse(code = 400, message = "Author not found error code.")
+    })
+    AuthorDTO findById(Long id);
 }
